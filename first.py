b85f5a757a74a851e3f7b8658f306d84ffa0fcf0
AswiniChejeti <ashwinikallam@gmail.com>
2026-02-17 21:57:17 +0530

    code posing

--- first.py
+++ first.py
@@ -1,10 +1,8 @@
-'''
-n=int(input("enter a range : "))
-list_of_numbers=[]
-for i in range(1,n+1):
-    list_of_numbers.append(i)
-print(list_of_numbers)
-'''
-l=[0, 2, 3, 4, 5, 6, 7, 8, 9, [12, 34, 45, [12, 54, [100, 101]]],10, 11, 12, 12, 91]
+#Infinite loop + break
+while True:          # infinite loop
+    user_input = input("Type 'stop' to end: ")
 
-print(l[8][7])
+    if user_input == "stop":
+        break        # exits the loop
+
+    print("Looping...")
